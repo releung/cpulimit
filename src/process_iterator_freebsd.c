7e69cd4adf8d7398c32e37b506104667ec41c2cd
HiGarfield <HiGarfield@126.com>
2024-05-03 10:30:35 +0800

    fix FreeBSD build error

--- src/process_iterator_freebsd.c
+++ src/process_iterator_freebsd.c
@@ -24,6 +24,10 @@
 #ifndef __PROCESS_ITERATOR_FREEBSD_C
 #define __PROCESS_ITERATOR_FREEBSD_C
 
+#if defined(__STRICT_ANSI__) || !defined(__STDC_VERSION__) || (__STDC_VERSION__ < 199901L)
+#define inline
+#endif
+
 #include "process_iterator.h"
 #include <sys/sysctl.h>
 #include <sys/user.h>
